e83a6331bbe4b906da730b9f1d140db8dbca8cea
fos1e2 <jean-marc.brun@cegedim-activ.com>
2021-12-10 16:07:00 +0100

    Backend - gitignore

--- exec.cmd
+++ exec.cmd
@@ -1,1 +1,1 @@
-java -Dserver.port=6969 -jar c:\dev_demo\workspaces\appli-douleur\douleur-spring-rest\target\douleur-spring-rest.jar "--app.context=recette" "--spring.profiles.active=R7"
+java -Dserver.port=6969 -jar C:\dev_demo\git-repositories\douleur-backend\target\douleur-spring-rest.jar "--app.context=recette" "--spring.profiles.active=R7"
